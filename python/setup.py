df30d7d9418f557e2a7a87cba059802aa3267db8
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-07-19 22:55:58 +0200

    feat(python): add python folder

---
+++ python/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import io
+
+from setuptools import find_packages, setup
+
+
+readme = io.open('README.rst', 'r', encoding='utf-8').read()
+
+setup(
+    name='',
+    description='',
+    long_description=readme,
+    author='',
+    author_email='',
+    version='0.0.0',
+    packages=find_packages(),
+    tests_require=[],
+    setup_requires=[],
+    install_requires=[
+    ],
+    entry_points=""""""
+)
