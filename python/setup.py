72a60bbe194f52d9a8cea3ff726b40bbfb62f236
Jacopo Cascioli <jacopo@jacopocascioli.com>
2018-12-19 23:40:48 +0000

    feat(python): update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
 import io
-import os
-import sys
 
 from setuptools import find_packages, setup
 
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
-    sys.exit()
-
-
-readme = io.open('README.rst', 'r', encoding='utf-8').read()
+readme = io.open('README.md', 'r', encoding='utf-8').read()
 
 setup(
     name='',
