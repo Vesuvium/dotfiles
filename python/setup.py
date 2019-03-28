4d5111f00b96d910bcaa2c9e4a6f233f62bbd43c
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-03-28 14:45:10 +0100

    feat/python: update setup file

--- python/setup.py
+++ python/setup.py
@@ -11,15 +11,21 @@ setup(
     description='',
     long_description=readme,
     url='',
-    author='',
-    author_email='',
+    author='Jacopo Cascioli',
+    author_email='noreply@jacopocascioli.com',
     version='0.0.0',
     license='',
     packages=find_packages(),
-    tests_require=[],
+    tests_require=[
+        'pytest>=4.3.1',
+        'pytest-cov>=2.6.1',
+        'pytest-mock>=1.10.1'
+    ],
     setup_requires=[],
     install_requires=[
     ],
     classifiers=[],
-    entry_points=""""""
+    entry_points={
+        'console_scripts': ['package=package.Cli:Cli.main']
+    }
 )
